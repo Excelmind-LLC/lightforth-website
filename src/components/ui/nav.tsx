4f471799db178ede7262970c9245f36adcc4bbb7
oreoluwa212 <olabisiabolarin5@gmail.com>
2024-12-11 17:03:48 +0100

    Styled navbar

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -1,8 +1,8 @@
 import { Logo } from "./logo";
 
 export const Navbar = () => (
-  <nav className="absolute top-[74px] left-0 px-4 w-full flex items-center justify-between">
+  <nav className="absolute top-[74px] left-0 px-16 w-full flex items-center justify-between">
     <Logo />
-    <p className="text-black text-lg underline">Get help</p>
+    <p className="text-black text-lg font-medium underline underline-offset-2">Get help</p>
   </nav>
 );
