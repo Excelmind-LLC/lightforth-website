a95ed1145687f70b9c41be8aabff699356357d73
webdevtolu <tolu.adeyemo7@gmail.com>
2024-12-13 20:59:21 +0100

    fixed chart

--- src/components/question-view/results.tsx
+++ src/components/question-view/results.tsx
@@ -14,7 +14,12 @@ export const Results = ({ updateQuestion }: UpdateQuestionStep) => {
           than ever before. Check out what others have experienced.
         </Card.Text>
       </Card.Container>
-      <div className="w-full h-[401px] rounded-xl bg-[#F9F9F9] flex flex-col items-center">
+      <div className="w-full gap-4 py-3.5 rounded-xl bg-[#F9F9F9] flex flex-col items-center">
+        <img
+          src="/chart.svg"
+          alt="chart"
+          className="w-auto h-auto object-cover object-center"
+        />
         <div className="w-[508px] h-24 rounded-full bg-[#EAEAEA] flex items-center gap-3 justify-center">
           <p className="text-[#0A2A60] font-semibold text-[46.829px] leading-[68px] font-clash-grotesk">
             80%
